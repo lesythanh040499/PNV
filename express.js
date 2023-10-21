1e9c12d4185d4906e1113de4a933a99d8ed31bf3
Thanh <kentkillboss@gmail.com>
2023-10-21 17:16:10 +0700

    add

---
+++ express.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const http = require("http").createServer(app);
+
+app.use("/api", require("./routers/authRoute"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+const port = 3000;
+http.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
